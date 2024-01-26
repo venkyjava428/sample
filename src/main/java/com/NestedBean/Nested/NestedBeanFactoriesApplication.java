601dd2fd6f6b4e2973e141b30d119b87401ac186
venkyjava428 <venkateshkilari06@gmail.com>
2024-01-26 10:56:55 +0200

    project  Archtecture

---
+++ src/main/java/com/NestedBean/Nested/NestedBeanFactoriesApplication.java
@@ -0,0 +1,13 @@
+package com.NestedBean.Nested;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestedBeanFactoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestedBeanFactoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NestedBean/Nested/NestedBeanFactoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NestedBean.Nested;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestedBeanFactoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
