64037a2930c67015606d4572cc88dc0677ca7c0e
venkyjava428 <130391712+venkyjava428@users.noreply.github.com>
2024-01-26 11:08:44 +0200

    Update NestedBeanFactoriesApplication.java
    h

--- src/main/java/com/NestedBean/Nested/NestedBeanFactoriesApplication.java
+++ src/main/java/com/NestedBean/Nested/NestedBeanFactoriesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NestedBeanFactoriesApplication {
 
 	public static void main(String[] args) {
+		System.out.println("sample");
 		SpringApplication.run(NestedBeanFactoriesApplication.class, args);
 	}
 
